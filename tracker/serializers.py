9208d5cbad50d41bd96a57c4f0ddf98b50287217
Yousef <yousef.ah@outlook.com>
2026-04-23 16:09:54 +0100

    Update serializers docstring

--- tracker/serializers.py
+++ tracker/serializers.py
@@ -1,18 +1,12 @@
-'''Serializers help validate incoming data and handle the conversion of JSON to Python objects.'''
+'''Serializers handle the conversion between JSON and Python objects and validate incoming data.'''
 
 from rest_framework import serializers
 from .models import Location, Satellite, Pass
 
-'''
-When a request comes in with JSON data, or when your API needs to send data back as JSON, 
-something needs to handle the conversion between JSON and Python objects — and also 
-validate incoming data. That's what a serializer does. For each model, you'll have a corresponding 
-serializer. DRF's ModelSerializer makes this straightforward — you just 
-tell it which model it's for and which fields to include.
-'''
-
 
 class LocationSerializer(serializers.ModelSerializer):
+    # The Meta class is a configuration block inside a class that defines how 
+    # that class should behave or be interpreted by the framework.
     class Meta:
         model = Location
         fields = '__all__'
